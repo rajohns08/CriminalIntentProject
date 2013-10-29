b72e81e1328971105aa448ed3c6d6e4813a5293d
Adam Johns <robert.adam.johns@gmail.com>
2013-10-29 10:43:17 -0500

    test

--- CriminalIntent/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ CriminalIntent/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -1,5 +1,5 @@
 package com.bignerdranch.android.criminalintent;
-//ABS BRANCH!! FIXING IGNORE AGAIN
+//ABS BRANCH!! FIXING IGNORE AGAIN TEST
 
 import android.annotation.TargetApi;
 import android.app.Activity;
