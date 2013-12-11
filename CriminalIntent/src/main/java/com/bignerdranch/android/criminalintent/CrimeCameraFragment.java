fbc653c97a637e436a9808f775f3adb9ababd4be
Adam Johns <robert.adam.johns@gmail.com>
2013-12-11 10:27:43 -0600

    added camera acquisition and release

--- CriminalIntent/src/main/java/com/bignerdranch/android/criminalintent/CrimeCameraFragment.java
+++ CriminalIntent/src/main/java/com/bignerdranch/android/criminalintent/CrimeCameraFragment.java
@@ -1,9 +1,12 @@
 package com.bignerdranch.android.criminalintent;
 
+import android.annotation.TargetApi;
 import android.hardware.Camera;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,6 +22,7 @@ public class CrimeCameraFragment extends Fragment {
     private SurfaceView surfaceView;
 
     @Override
+    @SuppressWarnings("deprecation")
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_crime_camera, parent, false);
         Button takePictureButton = (Button)v.findViewById(R.id.crime_camera_takePictureButton);
@@ -31,7 +35,30 @@ public class CrimeCameraFragment extends Fragment {
         });
 
         surfaceView = (SurfaceView)v.findViewById(R.id.crime_camera_surfaceView);
+        SurfaceHolder holder = surfaceView.getHolder();
+        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
 
         return v;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD) {
+            camera = Camera.open(0);
+        }
+        else {
+            camera = Camera.open();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        if (camera != null) {
+            camera.release();
+            camera = null;
+        }
     }
 }
